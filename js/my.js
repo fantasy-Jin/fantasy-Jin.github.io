5381a894db6956d50743c2097521711b066b82e6
fantasy <1768606916@qq.com>
2022-08-10 21:51:43 +0800

    Site updated: 2022-08-10 21:51:42

--- js/my.js
+++ js/my.js
@@ -1,9 +1,9 @@
 let backimg = [
-	// 随机图从这里开始
-	"url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/10.jpg)",
-	"url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/background.jpg)",
-	"url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/wallhaven-y82w7l.png)",
-	"url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/wallhaven-mplg21.png)",
+    // 随机图从这里开始
+    "url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/10.jpg)",
+    "url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/background.jpg)",
+    "url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/wallhaven-y82w7l.png)",
+    "url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/wallhaven-mplg21.png)",
     "url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/wallhaven-4dm75m.png)",
     "url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/wallhaven-m9g9v8.png)",
     "url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/wallhaven-4gr7p3.png)",
@@ -11,12 +11,12 @@ let backimg = [
     "url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/wallhaven-955l51.jpg)",
     "url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/wallhaven-mdqzoy.jpg)",
     "url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/wallhaven-w85vkp.jpg)",
-	
-   
+
+
 ];
-function getRandom(min, max){
-	return Math.floor(Math.random() * (max - min + 1) + min);
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
-let index = getRandom(0,backimg.length-1)
-console.log(backimg);
+let index = getRandom(0, backimg.length - 1)
+// console.log(backimg);
 document.querySelector("#web_bg").style.backgroundImage = backimg[index]
