b9ec72ffb6f7bd9a0444eb9b593b2d5c08157d00
fantasy <1768606916@qq.com>
2022-08-15 18:45:58 +0800

    Site updated: 2022-08-15 18:45:58

--- js/my.js
+++ js/my.js
@@ -1,22 +1,17 @@
 let backimg = [
-    // 随机图从这里开始
-    "url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/10.jpg)",
-    "url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/background.jpg)",
-    "url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/wallhaven-y82w7l.png)",
-    "url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/wallhaven-mplg21.png)",
-    "url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/wallhaven-4dm75m.png)",
-    "url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/wallhaven-m9g9v8.png)",
-    "url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/wallhaven-4gr7p3.png)",
-    "url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/wallhaven-9m7wr1.jpg)",
-    "url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/wallhaven-955l51.jpg)",
-    "url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/wallhaven-mdqzoy.jpg)",
-    "url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/wallhaven-w85vkp.jpg)",
-
-
+	// 随机图从这里开始
+	"url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/wallhaven-w85vkp.webp)",
+	"url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/wallhaven-mplg21.webp)",
+	"url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/wallhaven-m9g9v8.webp)",
+	"url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/background.webp)",
+	"url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/10.webp)",
+	"url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/1.webp)",
+	"url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/_20220805185049.webp)",
+	"url(https://picgo-fantasy06.oss-cn-guangzhou.aliyuncs.com/img/wallhaven-mdqzoy.webp)",
 ];
 function getRandom(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+	return Math.floor(Math.random() * (max - min + 1) + min);
 }
-let index = getRandom(0, backimg.length - 1)
+let index = getRandom(0, backimg.length - 1);
 // console.log(backimg);
-document.querySelector("#web_bg").style.backgroundImage = backimg[index]
+document.querySelector("#web_bg").style.backgroundImage = backimg[index];
